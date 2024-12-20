34646c263f775562c5871e3723ee1fcd415c1dda
Md. Sajjad Hossain <102672264+SajjadHossain0@users.noreply.github.com>
2024-12-20 20:01:36 +0600

    Update NewtonRaphsonMethod.java

--- NewtonRaphsonMethod.java
+++ NewtonRaphsonMethod.java
@@ -1,43 +1,48 @@
 import java.util.Scanner;
 
-public class NewtonRaphsonMethod {
+class NewtonRaphsonMethod{
 
-    public static double function(double x) {
-        return x * x * x - x - 1;
+    public static double f(double x){
+        return x*x*x - x - 1;
+    }
+    public static double derivative(double x){
+        return 3*x*x - 1;
     }
 
-    public static double derivative(double x) {
-        return 3 * x * x - 1;
-    }
+    public static void newtonRaphson(double a, double b, double accuracy){
+        if(f(a) * f(b) >= 0){
+            System.out.println("The value of a and b is invalid, please try again");
+            return;
+        }
 
-    public static void newtonRaphson(double a, double b, double accuracy) {
-
-        double x0 = (a + b) / 2, x1;
+        double x0 = (a + b) / 2,x1;
         int iteration = 0;
 
-        do {
+        do{
             iteration++;
-            x1 = x0 - function(x0) / derivative(x0);
 
-            System.out.println("Iteration " + iteration + ": x = " + x1 + ", f(x) = " + function(x1));
+            x1 = x0 - (f(x0) / derivative(x0));
+
+            System.out.println("Current root value is "+x1+", f(x0) = "+f(x0));
 
             x0 = x1;
-        } while (!(function(x1) <= accuracy && function(x1) >= -accuracy));
+        }
+        while(!(f(x1) <= accuracy && f(x1) >= -accuracy));
 
         System.out.println("The root is approximately: x = " + x1);
-        System.out.println("Found after " + iteration + " iterations.");
+        System.out.println("In the "+iteration+" iterations.");
+
     }
+
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter the initial a : ");
+        System.out.print("Enter the value of a = ");
         double a = input.nextDouble();
-
-        System.out.print("Enter the initial b : ");
+        System.out.print("Enter the value of b = ");
         double b = input.nextDouble();
-
-        System.out.print("Enter the accuracy (e.g., 0.0001): ");
+        System.out.print("Enter the value of accuracy = ");
         double accuracy = input.nextDouble();
 
         newtonRaphson(a, b, accuracy);
